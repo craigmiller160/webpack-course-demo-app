3e40dcee97eebd3c14c063df419ea04d3d2c5a7c
Craig Miller <craigmiller160@gmail.com>
2019-03-17 15:34:17 -0400

    Added more robust path resolving

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: path.resolve(__dirname, 'src'),
     output: {
         filename: 'main.[contentHash].js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/template.html',
+            template: path.resolve(__dirname, 'src/template.html')
         })
     ],
     module: {
