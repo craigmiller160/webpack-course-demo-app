b50917faea585661cd9ef3ca75e1deea8c006677
Craig Miller <craigmiller160@gmail.com>
2019-03-17 15:40:40 -0400

    Separated out dev and prod webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -2,12 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: path.resolve(__dirname, 'src'),
-    output: {
-        filename: 'main.[contentHash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'src/template.html')
